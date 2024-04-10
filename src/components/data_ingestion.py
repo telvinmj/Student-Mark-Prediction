e8836ddcf4c88fcc702322ac6fe90335afb47f91
Telvin Jose <telvin@Telvins-MacBook-Air.local>
2024-04-11 00:12:38 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,7 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 
 @dataclass
@@ -50,8 +51,13 @@ class DataIngestion:
             raise CustomException(e,sys)
 
 
-obj = DataIngestion()
-train_data,test_data=obj.intitiate_data_ingestion()
+if __name__=="__main__":
+    obj = DataIngestion()
+    train_data,test_data=obj.intitiate_data_ingestion()
 
-data_transformation = DataTransformation()
-data_transformation.initiate_data_transformation(train_data,test_data)
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    
